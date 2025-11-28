e6b5b1687e6f84a22c014100d6b50974a996865f
j2rkan <jerkan22@hotmail.com>
2025-11-28 13:43:50 -0500

    arreglando el script

--- clevercloud/run.sh
+++ clevercloud/run.sh
@@ -3,4 +3,6 @@
 
 export ConnectionStrings__DefaultConnection="Host=$POSTGRESQL_ADDON_HOST;Database=$POSTGRESQL_ADDON_DB;Username=$POSTGRESQL_ADDON_USER;Password=$POSTGRESQL_ADDON_PASSWORD;Port=$POSTGRESQL_ADDON_PORT"
 
-dotnet Firmeza.Api.dll
+
+
+dotnet run --project Firmeza.Api/Firmeza.Api.csproj --urls "http://0.0.0.0:8080"
